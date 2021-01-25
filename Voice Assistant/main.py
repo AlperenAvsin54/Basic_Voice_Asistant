a0a3e5e9a39c98220b202155f12817a80a496e5f
Alperen Avşin <73645785+AlperenAvsin54@users.noreply.github.com>
2021-01-25 21:12:18 +0300

    Add files via upload

--- Voice Assistant/main.py
+++ Voice Assistant/main.py
@@ -1,23 +1,46 @@
-# @author AlperenAvşin
-# girhub = https://github.com/AlperenAvsin54
+#@author AlperenAvşin
+#github = https://github.com/AlperenAvsin54
 
-from functions import record,speak,askQuestion,search,chat
+from functions import record,speak,hour,search
+import random
+import tkinter as tk
+from tkinter.ttk import *
 
-speak("Sana nasıl yardım edebilirim?")
+nasilsinList = ['nasılsın','iyi misin','nasıl gidiyor']
+nasilsinCevap = ['Sağ ol iyiyim','İyiyim','Çok iyiyim']
+tesekkürList = ['teşekkürler','teşekkür ederim','Sağ ol']
+tesekkürCevap = ['Bir şey değil','Beğendiğine sevindim']
+aramaList = ['arama yap','arama yaparmısın','internette ara','internette ararmısın']
+cıkısList = ['çıkış yap','kendini kapat','görüşürüz','görüşmek üzere','bay bay','sonra görüşürüz']
 
-while True:  ## Fonksiyonların çalıştığı kısım. Durmaması için while dögüsüne aldım.
-    voice = record()
-    print(voice)
+def message():
+    speak("Sana nasıl yardım edebilirim?")
+    while True:  ## Fonksiyonların çalıştığı kısım. Durmaması için while döngüsüne aldım.
+        voice = record()
+        print(voice)
 
-    if 'sohbet' in voice:
-        chat()
+        if voice in nasilsinList:
+            speak(random.choice(nasilsinCevap))
 
-    if 'arama yap' in voice:
-        search()
+        if voice in tesekkürList:
+            speak(random.choice(tesekkürCevap))
 
-    if 'soru' in voice:
-        askQuestion()
+        if voice in aramaList:
+            search()
 
-    if 'çıkış yap' in voice:
-        speak("Görüşürüz")
-        exit()
+        if 'saat kaç' in voice:
+            hour()
+
+        if voice in cıkısList:
+            speak("Görüşürüz")
+            exit()
+
+window = tk.Tk()
+window.title("Sesli Asistan")
+window.geometry("400x200")
+window.resizable(False,False)
+yazi = tk.Label(window,text="Konuşmak için butona bas!", fg="black", font=("Open Sans", "20", "bold"))
+buton = tk.Button(window,text="Konuş!", command=message, width="30", height="10")
+yazi.pack()
+buton.pack()
+tk.mainloop()
